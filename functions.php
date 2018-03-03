d5b2fbe3c804671083c684fe90287bfe78bc4792
jeremycherfas <jcherfas@mac.com>
2018-03-03 20:38:43 +0100

    Fix image header dimensions

--- functions.php
+++ functions.php
@@ -9,13 +9,36 @@ function my_theme_enqueue_styles() {
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
+
+
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+
 
 function unspam_webmentions($approved, $commentdata) {
   return $commentdata['comment_type'] == 'webmention' ? 1 : $approved;
 }
 
 add_filter('pre_comment_approved', 'unspam_webmentions', '99', 2);
+
+/* Change default dimensions of header image */
+/* from miklb */
+
+    function zenpress_setup()
+    {
+
+        $fornacalia_header_args = array(
+            'width'       => 1024,
+            'height'      => 350,
+            'header-text' => true,
+        );
+  
+        add_theme_support('custom-header', $fornacalia_header_args);
+
+  }
+/* un zenpress_setup() when the 'after_setup_theme' hook is run */
+
+add_action('after_setup_theme', 'zenpress_setup');
 
 ?>
